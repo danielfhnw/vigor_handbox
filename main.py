730163716778116ebc5a82260e2c670b0b6d0937
danielfhnw <daniel.jakober@fhnw.ch>
2026-05-22 08:59:55 +0200

    removed hmi_vend_soll from init()

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ import Statemachine
 
 def init():
     if not MotorAPI.get_inversion()[0]:
-        Statemachine.vend_soll = MotorAPI.get_vend()[0] - 100
-        RedisAPI.set_value("hmi_vend_soll", get_str(Statemachine.vend_soll))
+        Statemachine.vend_soll = MotorAPI.get_vend()[0]
     else:
         Statemachine.vend_soll = 910 - MotorAPI.get_vend()[0]
-        RedisAPI.set_value("hmi_vend_soll", get_str(910 - Statemachine.vend_soll))
     RedisAPI.set_value("hmi_state", "INIT")
 
 def update():
